693a0abec1caf483d8caf255bc59c092397c452b
Harry Ermawan <harryermawan@gmail.com>
2019-01-12 20:45:07 +0700

    ignore db setup

---
+++ config/example-database-config.php
@@ -0,0 +1,13 @@
+<?php
+ /* $koneksi=mysql_connect("localhost","root","");
+  mysql_select_db("fr_shoesstoredb",$koneksi);
+  $data_perpage=5;
+    rename this example to database_config.php
+*/
+  
+  $db_server="your hostname or server";
+  $db_user="your username db";
+  $db_password="your password";
+  $db_selected="your db";
+  $db_port="3306";
+?>
